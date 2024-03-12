0ed211c1cb05ffc23411b31d6a555aa661e07c51
Olga Voznaya <voznaya.olga@yandex.ru>
2024-03-12 21:05:34 +0300

    Вторая версия

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -4,8 +4,8 @@ from .views import category, about, rules
 app_name = 'pages'
 
 urlpatterns = [
-    path('about/', about, name = 'about'),
-    path('category/', category, name = 'category'),
-    path('rules/', rules, name = 'rules'),
+    path('about/', about, name='about'),
+    path('category/', category, name='category'),
+    path('rules/', rules, name='rules'),
 
 ]
